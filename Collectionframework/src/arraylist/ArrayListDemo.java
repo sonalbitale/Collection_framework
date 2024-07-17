de5b81c547b03d7d8a2b4de6a6a03a61db367487
sonalbitale <sonalbitale93@gmail.com>
2024-07-17 11:42:22 +0530

    by sonal: collection_framework

---
+++ Collectionframework/src/arraylist/ArrayListDemo.java
@@ -0,0 +1,17 @@
+package arraylist;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+	public static void main(String args[]) 
+	{
+	ArrayList<String> list = new ArrayList<>();
+	list.add("101");
+	list.add("sonal");
+	list.add("student");
+	System.out.println(list);}
+	
+	
+	
+
+}
